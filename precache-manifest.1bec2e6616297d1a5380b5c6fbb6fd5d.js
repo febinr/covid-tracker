771580f561da7ba055427a2024d37794e2f731df
Rethnakaran <frethna@apac.corpdir.net>
2020-05-05 10:41:39 +0530

    Updates

---
+++ precache-manifest.1bec2e6616297d1a5380b5c6fbb6fd5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86da1c8b5b5cbe8c93b44c6fc05d056",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f7582382b1f6ffb38e93",
+    "url": "/covid-tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fde82d5e437348096b0f",
+    "url": "/covid-tracker/static/js/2.1cf9fdde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-tracker/static/js/2.1cf9fdde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7582382b1f6ffb38e93",
+    "url": "/covid-tracker/static/js/main.76dfff69.chunk.js"
+  },
+  {
+    "revision": "1ee3619b46a3284fdd0e",
+    "url": "/covid-tracker/static/js/runtime-main.d20c9eb4.js"
+  }
+]);
